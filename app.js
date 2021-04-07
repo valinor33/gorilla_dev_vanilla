12cc1df9efaf061a208b0b8980a3ce39c622d63f
valinor <andresfullstack@gmail.com>
2021-04-07 13:53:24 -0300

    navbar action show / hide styles

---
+++ app.js
@@ -0,0 +1,6 @@
+function cambiarClase() {
+  let siteNav = document.getElementById("navbar");
+  siteNav.classList.toggle("navbar_open");
+  let menuOpen = document.getElementById("menu-toggle");
+  menuOpen.classList.toggle("menu-open");
+}
